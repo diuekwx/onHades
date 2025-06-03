6ee67952bb929c3408402e3ec78e7b886c424564
Jerison <jerisonw@gmail.com>
2025-06-03 06:36:29 -0700

    init commit

---
+++ roguelike/src/main/java/yeri/roguelike/RoguelikeApplication.java
@@ -0,0 +1,13 @@
+package yeri.roguelike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoguelikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoguelikeApplication.class, args);
+	}
+
+}
