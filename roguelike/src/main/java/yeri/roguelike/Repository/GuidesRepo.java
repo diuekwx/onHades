d464d1cf3b415cbedb742837a04e3d2becca2f60
Jerison <jerisonw@gmail.com>
2025-06-07 04:15:51 -0700

    return guides entity lol

--- roguelike/src/main/java/yeri/roguelike/Repository/GuidesRepo.java
+++ roguelike/src/main/java/yeri/roguelike/Repository/GuidesRepo.java
@@ -3,12 +3,13 @@ package yeri.roguelike.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import yeri.roguelike.Entity.Guides;
+import yeri.roguelike.Entity.Users;
 
 import java.util.List;
 
 @Repository
 public interface GuidesRepo extends JpaRepository<Guides, Long> {
-    List<String> findAllByAuthor(String author);
-    String findByAuthor(String author);
+    List<Guides> findAllByAuthor(Users author);
+    Guides findByAuthor(Users author);
 
 }
